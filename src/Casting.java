9557285740a80a3ffc4d1813bdfb9b2f8b92c5c7
Daniel Vinicius <danielvinicius100@hotmail.com>
2024-02-27 21:50:02 -0300

    commit inicial

---
+++ src/Casting.java
@@ -0,0 +1,11 @@
+public class Casting {
+    public static void main (String[] args) {
+        System.out.println("Lendo o numero 1:");
+        int numero1 = 10;
+    System.out.println(numero1);
+
+    System.out.println("Lendo o wrapper do numero 1:");
+    Integer wrapperNumero1 = numero1;
+        System.out.println(wrapperNumero1);
+    }
+}
